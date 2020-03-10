19e848d93c549330263aed101ddd660902afd2b2
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-10 19:49:12 +0300

    First update of task1
    Now it looks better

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
-﻿#region Fog
+#region Hidden
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1
+namespace задание_1_курс
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             #endregion
-            Console.WriteLine("Утро. Поликлиника. Вы в очереди за справкой.");
-            Console.Write("Казалось бы - что может пойти не так? Но вот вы видите старушек." +
-                "Сколько их?"); 
 
-            int countOldLadies = Convert.ToInt32 (Console.ReadLine());
-            Console.WriteLine("Их действительно " + countOldLadies);
+            int costOfFood = 5, coinsInTheWallet;
+            bool isAnswerRight, LeaveGame;
+            float sizeOfShoose;
+            uint usersCount;
+            char lifesCount = '\u2665';
+            string userNane, placeOfBirht, eyeColor; 
 
-            int hourseWaiting = (countOldLadies * 10) / 60;
-            int minutesWaiting = (countOldLadies * 10) % 60;
-
-            Console.WriteLine($"И вы понимаете, что ждать вам осталось {hourseWaiting} часов {minutesWaiting} минут...");
+#region Hidden
         }
     }
 }
+#endregion 
