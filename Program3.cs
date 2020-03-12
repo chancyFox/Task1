8222954c002dbdcf234f7a5ce242ee7859a8de3c
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-12 20:37:22 +0300

    File added

---
+++ Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3_введение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imageCount = 56;
+            int imagesInLine = 3;
+
+            int lines = imageCount / imagesInLine;
+            int imageLeft = imageCount % imagesInLine; 
+
+            Console.WriteLine($"У вас имеется {imageCount} картинок. ");
+            Console.WriteLine($"Из них можно составить {lines} рядов. {imageLeft} картинки осталось");
+        }
+    }
+}
