2b0d103252268bf990c92d863cc51b50eb3c994a
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-13 10:29:02 +0300

    Update and rename Program3.cs to Task6.cs

---
+++ Task6.cs
@@ -0,0 +1,15 @@
+    string userName, userAge, userZodiaс, userWork;
+
+    Console.Write($"Здравствуй! Как тебя зовут? \n- ");
+    userName = Console.ReadLine();
+
+    Console.Write($"Сколько тебе лет? \n- ");
+    userAge = Console.ReadLine();
+
+    Console.Write($"Кто ты по знаку зодиака? \n- ");
+    userZodiaс = Console.ReadLine();
+
+    Console.Write($"Где ты работаешь? \n- ");
+    userWork = Console.ReadLine();
+
+    Console.WriteLine($"Тебя зовут {userName}, тебе {userAge} лет, по знаку зодиака ты {userZodiaс} и работаешь {userWork}. ");
