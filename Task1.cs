7ecaa4543a4d70217de9947e456a16cff54583c1
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-13 10:31:09 +0300

    Update and rename Program.cs to Task1.cs

---
+++ Task1.cs
@@ -0,0 +1,6 @@
+    int costOfFood = 5, coinsInTheWallet;
+    bool isAnswerRight, LeaveGame;
+    float sizeOfShoose;
+    uint usersCount;
+    char lifesCount = '\u2665';
+    string userNane, placeOfBirht, eyeColor; 
