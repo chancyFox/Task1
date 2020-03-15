501640347c97cf598d0f1a851b56f1793b7dd446
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-15 20:45:17 +0300

    Add files via upload

---
+++ Loops and Conditional operators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string password = "12345"; 
+            int tryes = 3;
+            bool isPasswordCorrect = false;
+
+            while ((isPasswordCorrect != true) & (tryes != 0))
+            {
+                Console.WriteLine("Введите пароль!");
+                Console.Write("Пароль: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    isPasswordCorrect = true;
+                    Console.WriteLine("Авторизация прошла успешно. Добро пожаловать!");
+                }
+                else
+                {
+                    tryes -= 1;
+
+                    if (tryes == 0)
+                    {
+                        Console.WriteLine("Провал миссии. До самоуничтожения остаётся: 3... 2...");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Пароль не верный! Осторожнее, попыток осталось всего {tryes}.");
+                    }
+                }
+            }
+        }
+    }
+}
