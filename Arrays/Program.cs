499f060c324d9ca3dc01237e8eb767347289df74
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-19 23:16:22 +0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int biggestElement = int.MinValue;
+            int[,] array = new int[10, 10];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand.Next(10, 100);
+                    Console.Write(array[i, j] + " ");
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i,j] > biggestElement)
+                    {
+                        biggestElement = array[i, j];
+                    }
+                    if ((i == (array.GetLength(0) - 1)) && (j == (array.GetLength(1) - 1)))
+                    {
+                        Console.WriteLine("\n\nНаибольший элемент матрицы: " + biggestElement + "\n");
+                    }
+                }
+            }
+        }
+    }
+}
